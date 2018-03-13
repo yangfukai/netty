70e15d6043db5582b082131b662800057f1ebf92
yangfukai <yangfukai@zhonggu.cn>
2018-03-13 11:56:55 +0800

    s

---
+++ NettyHello/src/Three/com/cn/pool/Worker.java
@@ -0,0 +1,17 @@
+package Three.com.cn.pool;
+
+import java.nio.channels.SocketChannel;
+/**
+ * worker接口
+ * @author -琴兽-
+ *
+ */
+public interface Worker {
+	
+	/**
+	 * 加入一个新的客户端会话
+	 * @param channel
+	 */
+	 void registerNewChannelTask(SocketChannel channel);
+
+}
